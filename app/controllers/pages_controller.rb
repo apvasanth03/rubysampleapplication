ff7c4f3debd213c33a0654e6a7e1b1fc65dbedd3
apvasanth03 <apvasanth03@gmail.com>
2016-02-20 10:13:59 +0530

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,18 @@
 class PagesController < ApplicationController
+
+# Home	
   def home
+  	@title = "Home"
   end
 
+# Contact
   def contact
+  	@title = "Contact"
+  end
+
+# About
+  def about
+  	@title = "About"
   end
 
 end
