ad443598ca869bd8e5a7447bb0e223bbece4bc26
apvasanth03 <apvasanth03@gmail.com>
2016-03-08 07:43:30 +0530

    Added helper to get page title & Added BluePrint Css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	# Title - Used to get title for our webpage.
+	# If webpage has title then appends the title to base title and return it.
+	# If webpage doen't have title then just return base title.
+	def title
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+
 end
